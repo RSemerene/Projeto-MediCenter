156423419687cc49cef1f3c20030b221d39c2177
Ricardo Semerene <rsemerene@live.com>
2020-12-17 21:58:25 -0300

    ultima modif

---
+++ assets/JS/script.js
@@ -0,0 +1,8 @@
+function menu() {
+    let menu = document.getElementById("botaoMenu");
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+    } else menu.style.display = "none";
+    
+}
+
